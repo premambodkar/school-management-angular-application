07acc716e6c17830d0b86247fe11caf87a48301f
premambodkar <prem.ambodkar@gmail.com>
2023-01-25 00:00:07 +0530

    1 file modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { RouterModule } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
 
-const routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    children: [
+      {
+        path: 'school',
+        loadComponent: () =>
+          import('./sclass/sclass.component').then((m) => m.SclassComponent),
+      },
+      {
+        path: 'school',
+        loadComponent: () =>
+          import('./school/school.component').then((m) => m.SchoolComponent),
+      },
+      {
+        path: 'marks',
+        loadComponent: () =>
+          import('./marks/marks.component').then((m) => m.MarksComponent),
+      },
+    ],
+  },
+];
 
 @NgModule({
   imports: [BrowserModule, FormsModule, RouterModule.forRoot(routes)],
