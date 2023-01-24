39b922055e736059b2740a24e3abc70530074dc7
premambodkar <prem.ambodkar@gmail.com>
2023-01-25 01:59:30 +0530

    2 files modified, 1 deleted

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,58 @@
 import { Component, VERSION } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+
+import { RouterModule, Routes } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
+
+const routes: Routes = [
+  {
+    path: '',
+
+    children: [
+      {
+        path: '',
+        redirectTo: 'dashboard',
+        pathMatch: 'full',
+      },
+      {
+        path: 'dashboard',
+        loadComponent: () =>
+          import('./dashboard/dashboard.component').then(
+            (m) => m.DashboardComponent
+          ),
+      },
+      {
+        path: 'class',
+        loadComponent: () =>
+          import('./sclass/sclass.component').then((m) => m.SclassComponent),
+      },
+      {
+        path: 'school',
+        loadComponent: () =>
+          import('./school/school.component').then((m) => m.SchoolComponent),
+      },
+      {
+        path: 'marks',
+        loadComponent: () =>
+          import('./marks/marks.component').then((m) => m.MarksComponent),
+      },
+    ],
+  },
+];
 
 @Component({
   selector: 'my-app',
+  imports: [
+    BrowserModule,
+    FormsModule,
+    CommonModule,
+    RouterModule.forRoot(routes),
+    HttpClientModule,
+  ],
   templateUrl: './app.component.html',
+  standalone: true,
 })
 export class AppComponent {
   name = 'Angular ' + VERSION.major;
--- src/main.ts
+++ src/main.ts
@@ -4,13 +4,17 @@ import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
+import { bootstrapApplication } from '@angular/platform-browser';
+import { AppComponent } from './app/app.component';
 
-platformBrowserDynamic().bootstrapModule(AppModule).then(ref => {
-  // Ensure Angular destroys itself on hot reloads.
-  if (window['ngRef']) {
-    window['ngRef'].destroy();
-  }
-  window['ngRef'] = ref;
+bootstrapApplication(AppComponent)
+  .then((ref) => {
+    // Ensure Angular destroys itself on hot reloads.
+    if (window['ngRef']) {
+      window['ngRef'].destroy();
+    }
+    window['ngRef'] = ref;
 
-  // Otherwise, log the boot error
-}).catch(err => console.error(err));
+    // Otherwise, log the boot error
+  })
+  .catch((err) => console.error(err));
