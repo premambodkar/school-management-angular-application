304d83f14daca54db81cbc3c26b3f194d9afb0ff
premambodkar <prem.ambodkar@gmail.com>
2023-01-26 01:21:57 +0530

    2 files modified, 1 added

--- src/app/sclass/add-update-sclass/add-update-sclass.component.ts
+++ src/app/sclass/add-update-sclass/add-update-sclass.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute } from '@angular/router';
+import { SclassModel } from '../../common/sclass.model';
+import { SclassService } from '../sclass.service';
 
 @Component({
   selector: 'app-add-update-sclass',
@@ -6,7 +10,52 @@ import { Component, OnInit } from '@angular/core';
   standalone: true,
 })
 export class AddUpdateSclassComponent implements OnInit {
-  constructor() {}
+  @Input()
+  sclassId!: number;
 
-  ngOnInit() {}
+  sclass: SclassModel = new SclassModel();
+  myForm: FormGroup;
+
+  constructor(
+    public sclassService: SclassService,
+    private fb: FormBuilder,
+    private readonly activatedRoute: ActivatedRoute
+  ) {}
+
+  ngOnInit() {
+    this.myForm = this.fb.group({
+      name: ['', Validators.required],
+      location: ['', [Validators.required]],
+      divisions: ['', Validators.required],
+      noOfStudent: ['', [Validators.required]],
+    });
+    this.activatedRoute.queryParams.subscribe((params) => {
+      if (params['sclassId']) {
+        this.sclassId = params['sclassId'];
+        this.getSClass();
+      }
+    });
+  }
+
+  addUpdateSclass(myForm: FormGroup) {
+    if (myForm.valid) {
+      this.sclass.name = this.myForm.value['name'];
+      this.sclass.location = this.myForm.value['location'];
+      this.sclass.divisions = this.myForm.value['divisions'];
+      this.sclass.noOfStudent = this.myForm.value['noOfStudent'];
+      if (this.sclass.id) {
+        this.sclassService
+          .updateSClass(this.sclass)
+          .subscribe((resp: any) => {});
+      } else {
+        this.sclassService.addSClass(this.sclass).subscribe((resp: any) => {});
+      }
+    }
+  }
+
+  getSClass() {
+    this.sclassService.getSClass(this.sclassId).subscribe((resp) => {
+      this.sclass = Object.assign(resp);
+    });
+  }
 }
