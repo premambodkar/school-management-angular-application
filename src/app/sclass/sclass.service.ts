beb3de6a9aaa15d6e9e45bc3f0bce5a4d4d17d3c
premambodkar <prem.ambodkar@gmail.com>
2023-01-27 10:50:05 +0530

    1 file modified

--- src/app/sclass/sclass.service.ts
+++ src/app/sclass/sclass.service.ts
@@ -8,40 +8,57 @@ import { SclassModel } from '../common/sclass.model';
   providedIn: 'root',
 })
 export class SclassService {
+  sclassInfoList: SclassModel[] = [
+    {
+      id: 1,
+      name: 'CLass 1',
+      location: 'First Floor',
+      divisions: ['A'.'B','C'],
+      noOfStudent: 150,
+    },
+  ];
   constructor(
     private readonly http: HttpClient,
     private readonly commonService: CommonService
   ) {}
 
   getAllSClass(): Observable<any[]> {
-    return of([
-      {
-        id: 1,
-        name: 'CLass 1',
-        location: 'First Floor',
-        divisions: 5,
-        noOfStudent: 150,
-      },
-    ]);
+    return of(this.sclassInfoList);
   }
 
-  getSClass(schoolId: number): Observable<any> {
-    const url = `${this.commonService.url}/getSchool?schoolId=${schoolId}`;
-    return this.http.get(url);
+  getSClass(sclassId: number): Observable<any> {
+    return of(this.sclassInfoList.find((sclassInfo: SclassModel) => {
+      sclassInfo.id === sclassId;
+    }));
+    // const url = `${this.commonService.url}/getSClass?schoolId=${schoolId}`;
+    // return this.http.get(url);
   }
 
-  addSClass(schoolInfo: SclassModel): Observable<any> {
-    const url = `${this.commonService.url}/addSchool`;
-    return this.http.post(url, schoolInfo);
+  addSClass(sclassInfo: SclassModel): Observable<any> {
+    return of(this.sclassInfoList.push(sclassInfo));
+    // const url = `${this.commonService.url}/addSClass`;
+    // return this.http.post(url, sclassInfo);
   }
 
-  updateSClass(schoolInfo: SclassModel): Observable<any> {
-    const url = `${this.commonService.url}/updateSchool`;
-    return this.http.put(url, schoolInfo);
+  updateSClass(sclassInfo: SclassModel): Observable<any> {
+    return of(this.sclassInfoList.forEach((item: SclassModel)=>{
+if(item.id === sclassInfo.id){
+  item.location = sclassInfo.location;
+  item.name = sclassInfo.name;
+  item.noOfStudent = sclassInfo.noOfStudent;
+  item.divisions = sclassInfo.divisions;
+}
+    }));
+    // const url = `${this.commonService.url}/updateSClass`;
+    // return this.http.put(url, sclassInfo);
   }
 
-  deleteSClass(schoolId: number): Observable<any> {
-    const url = `${this.commonService.url}/getSchool?schoolId=${schoolId}`;
+  deleteSClass(sclassId: number): Observable<any> {
+    const index = this.sclassInfoList.findIndex((sclassInfo: SclassModel) => {
+      sclassInfo.id === sclassId;
+    });
+    return of(this.sclassInfoList.splice(index,1));
+    const url = `${this.commonService.url}/deleteSClass?sclassId=${sclassId}`;
     return this.http.delete(url);
   }
 }
